b2d0f9914938aad88ff6c7956a0b73e79a997a83
Saqlain-Rasheed <msaqi681@gmail.com>
2020-05-15 21:22:40 +0500

    done day 16

--- reactjs_15days/src/App.js
+++ reactjs_15days/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 import Testing from './components/Testing';
 
+
+
 function App() {
   return (
     <React.Fragment>
+    
       <h1>Hello world</h1>
       <Testing />
     </React.Fragment>
--- reactjs_15days/src/components/Testing.js
+++ reactjs_15days/src/components/Testing.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
+
 class Testing extends React.Component{
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
   constructor(props){
     super(props);
     this.state = {
-      date: new Date()
+      date:new Date(),
     }
   }
+  
   render(){
     return (  
       <div>
